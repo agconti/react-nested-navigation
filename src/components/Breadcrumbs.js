63aeadef2d1e5ddbd374986f8a8e1b88dfdfb190
agconti <agc11d@gmail.com>
2017-04-30 16:03:01 -0400

    Revert "feat(app): update laptop styles"
    This reverts commit 1711d41037560306bfa0cea322328aae345540ed.

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Breadcrumbs = ({items}) => (
-  <h1 className="breadcrumbs">{items.join(' / ')}</h1>
+  <h1>{items.join(' / ')}</h1>
 )
 
 export default Breadcrumbs
